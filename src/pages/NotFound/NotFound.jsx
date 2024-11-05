b67c87f6d8cb6e56633e8d5fc18b4b2b7a640465
IsmaSEO <ismail.jelassi.dev@gmail.com>
2024-11-05 09:55:18 +0100

    creation component not found et page not found desktop + mobile

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,18 @@
+// src/pages/NotFound/NotFound.jsx
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 function NotFound() {
   return (
-    <div>
-      <h1>Page non trouvée</h1>
+    <div className="not-found-container">
+      <h1 className="not-found-title">404</h1>
+      <h2 className="not-found-subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/" className="not-found-link">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
