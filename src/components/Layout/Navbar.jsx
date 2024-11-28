34fbd8d16c4f0ef5b5c610739c92eba53f8974a8
IsmaSEO <ismail.jelassi.dev@gmail.com>
2024-11-28 09:04:52 +0100

    fix nav non souligné avec navlink

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,6 +1,6 @@
 // src/components/Layout/Navbar.jsx
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
@@ -8,16 +8,26 @@ function Navbar() {
     <header className="site-header">
       <nav className="site-nav">
         <div className="site-logo">
-          <Link to="/">
+          <NavLink to="/">
             <img src="/logoKasa.svg" alt="Logo Kasa" />
-          </Link>
+          </NavLink>
         </div>
         <ul className="nav-list">
           <li className="nav-item">
-            <Link to="/" className="nav-link">Accueil</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
+            >
+              Accueil
+            </NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link">À Propos</Link>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
+            >
+              À Propos
+            </NavLink>
           </li>
         </ul>
       </nav>
